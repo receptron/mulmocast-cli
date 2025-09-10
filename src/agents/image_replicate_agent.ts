feb910ea431bc2c40a5159b1c45836da04d20cdf
snakajima <satoshi.nakajima@gmail.com>
2025-09-10 11:02:49 -0700

    image2image

--- src/agents/image_replicate_agent.ts
+++ src/agents/image_replicate_agent.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from "fs";
 import { AgentFunction, AgentFunctionInfo, GraphAILogger } from "graphai";
 import Replicate from "replicate";
 import { getAspectRatio } from "./movie_replicate_agent.js";
@@ -13,7 +14,7 @@ export const imageReplicateAgent: AgentFunction<ReplicateImageAgentParams, Agent
   params,
   config,
 }) => {
-  const { prompt } = namedInputs;
+  const { prompt, referenceImages } = namedInputs;
   const { canvasSize } = params;
   const model = params.model ?? (provider2ImageAgent.replicate.defaultModel as `${string}/${string}`);
   const apiKey = config?.apiKey;
@@ -27,24 +28,14 @@ export const imageReplicateAgent: AgentFunction<ReplicateImageAgentParams, Agent
   const input = {
     prompt,
     aspect_ratio: getAspectRatio(canvasSize),
-  };
+  } as { prompt: string; aspect_ratio: string; image_input?: string[] };
 
-
-  // Add image if provided (for image-to-image generation)
-  /*
-  if (imagePath) {
-    const buffer = readFileSync(imagePath);
-    const base64Image = `data:image/png;base64,${buffer.toString("base64")}`;
-    const start_image = provider2MovieAgent.replicate.modelParams[model]?.start_image;
-    if (start_image === "first_frame_image" || start_image === "image" || start_image === "start_image") {
-      input[start_image] = base64Image;
-    } else if (start_image === undefined) {
-      throw new Error(`Model ${model} does not support image-to-video generation`);
-    } else {
-      input.image = base64Image;
-    }
+  if (referenceImages && referenceImages.length > 0) {
+    input.image_input = referenceImages.map((image) => {
+      const buffer = readFileSync(image);
+      return `data:image/png;base64,${buffer.toString("base64")}`;
+    });
   }
-  */
 
   try {
     const output = await replicate.run(model, { input });
