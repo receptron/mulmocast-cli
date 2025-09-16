98a0ca5a302ae4f04798c7ff346de747e2bc18ec
isamu <isamu@to-kyo.to>
2025-09-16 20:27:17 +0900

    update test

--- test/agents/test_combine_audio_files.ts
+++ test/agents/test_combine_audio_files.ts
@@ -2,8 +2,33 @@ import test from "node:test";
 import assert from "node:assert";
 
 import { createMockContext, createMockBeat } from "../actions/utils.js";
-
+import { createStudioData } from "../../src/utils/context.js";
 import { updateDurations } from "../../src/agents/combine_audio_files_agent.js";
+
+test("updateDurations audio duration", async () => {
+  const mediaDurations = [
+    {
+      movieDuration: 123,
+      audioDuration: 333,
+      hasMedia: false,
+      silenceDuration: 222,
+      hasMovieAudio: false,
+    },
+  ];
+  const beat = createMockBeat({
+    duration: 1,
+  });
+
+  const mock = createMockContext();
+  mock.studio.script["$mulmocast"] = {
+    version: "1.1",
+  };
+
+  mock.studio.script.beats.push(beat);
+  mock.studio = createStudioData(mock.studio.script, "test");
+  const res = updateDurations(mock, mediaDurations);
+  assert.strictEqual(res[0], 123);
+});
 
 test("updateDurations movie duration", async () => {
   const mediaDurations = [
@@ -44,3 +69,7 @@ test("updateDurations just beat duration", async () => {
   const res = updateDurations(mock, mediaDurations);
   assert.strictEqual(res[0], 123);
 });
+
+// TODO voice-over beat / movie
+
+// TODO spilled over audi / audio
