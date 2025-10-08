02721045d25034fc6e20ffa5ee2052a25575ccd9
isamu <isamu@to-kyo.to>
2025-10-08 19:42:50 +0900

    fix

--- src/utils/asset_import.ts
+++ src/utils/asset_import.ts
@@ -1,6 +1,7 @@
 import { createRequire } from "node:module";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
+import fs from "node:fs";
 
 const require = createRequire(import.meta.url);
 
