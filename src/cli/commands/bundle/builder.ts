e311488b8f0da23e36d89ee68f891d1bf2a895ed
isamu <isamu@to-kyo.to>
2025-10-15 16:01:20 +0900

    unused option

--- src/cli/commands/bundle/builder.ts
+++ src/cli/commands/bundle/builder.ts
@@ -2,7 +2,5 @@ import type { Argv } from "yargs";
 import { commonOptions } from "../../common.js";
 
 export const builder = (yargs: Argv) =>
-  commonOptions(yargs).option("image_width", {
-    describe: "Image width (e.g., 400px, 50%, auto)",
-    type: "string",
-  });
+  // zip
+  commonOptions(yargs);
